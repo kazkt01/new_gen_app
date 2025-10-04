f3ee126f2fee93db4c2916b86843e107577ae0ec
kazkt01 <diminishdanish@gmail.com>
2025-10-05 00:36:10 +0900

    Python復習

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 ✅Python & Fast API 
 ✅TypeScript & Next.js
 ✅Docker
-
 ✍️
 ・UIにこだわる
 ・CORSでAPIの叩きを制御
 ・AWSにデプロイできればやる
 ・QIITAにまとめる
+・CI/CDも構築したいかも？
 
 
 
--- python_review/review.py
+++ python_review/review.py
@@ -6,3 +6,28 @@ a = 1
 b = 2
 sum = a + b
 print(sum)
+
+# 関数
+def sum():
+    return a + b
+print(sum())
+# テンプレートリテラル "f"
+msg = "禁止区域です"
+def alert(msg):
+    return f"ここは、{msg}"
+print(alert(msg))
+
+age = 12
+
+if  age >= 20:
+    msg = "禁止区域です"
+    def alert(msg):
+        return f"ここは、{msg}"
+    hoge = alert(msg)
+    print(hoge)
+else:
+    msg = "安全区域です"
+    def alert(msg):
+        return f"ここは、{msg}"
+    hoge = alert(msg)
+    print(hoge)
